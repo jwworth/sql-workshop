8be4a4c69227feda0ed25182a6d3524a342d037a
Jack Christensen <jack@jackchristensen.com>
2021-02-22 17:03:41 -0600

    Update COVID-19 data

--- covid_19/solutions.sql
+++ covid_19/solutions.sql
@@ -1,12 +1,12 @@
 -- 1.1 How many total deaths have there been?
 select sum(death)
 from all_states_history
-where date = '2020-10-26';
+where date = '2021-02-21';
 
 -- 1.2 How many total tests have been performed?
 select sum(totaltestresults)
 from all_states_history
-where date = '2020-10-26';
+where date = '2021-02-21';
 
 -- 2.1 When was the peak ICU usage and how many were in use?
 select date, sum(inicucurrently)
@@ -18,14 +18,14 @@ limit 1;
 -- 2.2 How many patients are in the ICO now?
 select sum(inicucurrently)
 from all_states_history
-where date = '2020-10-26';
+where date = '2021-02-21';
 
--- 2.3 How many total patients have been in the ICU?
+-- 2.3 How many total patients have been in the ICU? (This number seems suspiciously low.)
 select sum(inicucumulative)
 from all_states_history
-where date = '2020-10-26';
+where date = '2021-02-21';
 
 --   2.4 How many total patients have been hospitalized?
 select sum(hospitalizedcumulative)
 from all_states_history
-where date = '2020-10-26';
+where date = '2021-02-21';
